9b65fa0150bb8f37ffbd21b4db5f7eaebbfd5b9d
NikoKavas <niko.kavas@student.um.si>
2025-04-21 22:45:12 +0200

    main

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './modules/app.module';
+import { ValidationPipe } from '@nestjs/common'
+import { NestFactory } from '@nestjs/core'
+import cookieParser from 'cookie-parser'
+import express from 'express'
+import Logging from './library/Logging'
+
+import { AppModule } from './modules/app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create(AppModule, {
+    bufferLogs: true,
+  })
+  app.enableCors({
+    origin: ['http://localhost:3000'],
+    credentials: true,
+  })
+
+  app.useGlobalPipes(new ValidationPipe())
+  app.use(cookieParser())
+
+  const PORT = process.env.PORT || 8080
+  await app.listen(PORT)
+
+  Logging.info(`App is listening on: ${await app.getUrl()}`)
 }
-bootstrap();
+bootstrap()
